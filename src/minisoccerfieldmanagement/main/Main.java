1328f39d8e8b9140bc591a92c1078d52e3e7ce9c
Khan9Tran <trankhangtctv@gmail.com>
2024-03-18 21:12:32 +0700

    sửa menu

--- src/minisoccerfieldmanagement/main/Main.java
+++ src/minisoccerfieldmanagement/main/Main.java
@@ -57,7 +57,7 @@ public class Main extends javax.swing.JFrame {
         revalidate();
         repaint();
     }
-
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
