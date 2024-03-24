8a2355235ea3c9cce66139348c76f7dcafb884cf
thanhhuy25 <nguyenhuythanh256@gmail.com>
2024-03-24 16:31:25 +0700

    Create Booking, Match model

---
+++ src/minisoccerfieldmanagement/model/Match.java
@@ -0,0 +1,70 @@
+package minisoccerfieldmanagement.model;
+
+import java.sql.Timestamp;
+
+public class Match {
+
+    private int id;
+    private int bookingId;
+    private Timestamp checkIn;
+    private Timestamp checkOut;
+    private boolean isDeleted;
+    private Timestamp createdAt;
+    private Timestamp updateAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Timestamp getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Timestamp checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Timestamp getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Timestamp checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public boolean isIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Timestamp updateAt) {
+        this.updateAt = updateAt;
+    }
+}
