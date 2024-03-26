e295b6584f7e9c7524ca886113ac13ee31ae6964
Khan9Tran <trankhangtctv@gmail.com>
2024-03-26 10:10:27 +0700

    thêm đồ chơi nữa

--- src/minisoccerfieldmanagement/form/ServiceManagement.java
+++ src/minisoccerfieldmanagement/form/ServiceManagement.java
@@ -5,6 +5,20 @@
 package minisoccerfieldmanagement.form;
 
 import com.formdev.flatlaf.FlatClientProperties;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import java.awt.Color;
+import java.awt.Component;
+import java.text.DecimalFormat;
+import java.util.Random;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 import minisoccerfieldmanagement.tabbed.TabbedForm;
 import minisoccerfieldmanagement.util.TableGradientCell;
 
@@ -19,19 +33,101 @@ public class ServiceManagement extends TabbedForm {
      */
     public ServiceManagement() {
         initComponents();
-        jTable1.setDefaultRenderer(Object.class, new TableGradientCell());
-        this.putClientProperty(FlatClientProperties.STYLE, ""
-                + "border:1,1,1,1,$TableHeader.bottomSeparatorColor,,10");
-        jTable1.getTableHeader().putClientProperty(FlatClientProperties.STYLE, ""
-                + "hoverBackground:null;"
-                + "pressedBackground:null;"
-                + "separatorColor:$TableHeader.background");
-        jScrollPane1.putClientProperty(FlatClientProperties.STYLE, ""
-                + "border:3,0,3,0,$Table.background,10,10");
-        jScrollPane1.getVerticalScrollBar().putClientProperty(FlatClientProperties.STYLE, ""
-                + "hoverTrackColor:null");
+        FlatLaf.registerCustomDefaultsSource("tableview");
+        applyTableStyle(jTable1);
+        testData(jTable1);
+    }
+    
+    private void testData(JTable table) {
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        model.addRow(getRandomRow("Beer"));
+        model.addRow(getRandomRow("Shirt"));
+        model.addRow(getRandomRow("Laptop"));
+        model.addRow(getRandomRow("Book"));
+        model.addRow(getRandomRow("Coffee"));
+        model.addRow(getRandomRow("Phone"));
+        model.addRow(getRandomRow("Chair"));
+        model.addRow(getRandomRow("Watch"));
+        model.addRow(getRandomRow("Sunglasses"));
+        model.addRow(getRandomRow("Bag"));
+        model.addRow(getRandomRow("Headphones"));
+        model.addRow(getRandomRow("Camera"));
+        model.addRow(getRandomRow("Necklace"));
+        model.addRow(getRandomRow("Perfume"));
+        model.addRow(getRandomRow("Wallet"));
+        model.addRow(getRandomRow("Jacket"));
+        model.addRow(getRandomRow("Bicycle"));
+        model.addRow(getRandomRow("Game console"));
+        model.addRow(getRandomRow("Tennis racket"));
+    }
+    private Object[] getRandomRow(String name) {
+        DecimalFormat df = new DecimalFormat("#,##0.##");
+        return new Object[]{false, name, "$" + df.format(getAmount(9999, 9999999)), "$" + df.format(getAmount(9999, 9999999)), df.format(getAmount(-100, 100))};
+    }
+    
+    private double getAmount(int from, int to) {
+        Random ran = new Random();
+        return (ran.nextInt(to - from) + from) * ran.nextDouble();
     }
 
+    private void applyTableStyle(JTable table) {
+
+        cmdAdd.setIcon(new FlatSVGIcon("minisoccerfieldmanagement/drawer/icon/add.svg", 0.35f));
+        cmdUpdate.setIcon(new FlatSVGIcon("minisoccerfieldmanagement/drawer/icon/edit.svg", 0.35f));
+        cmdDelete.setIcon(new FlatSVGIcon("minisoccerfieldmanagement/drawer/icon/delete.svg", 0.35f));
+
+        txtSearch.putClientProperty(FlatClientProperties.TEXT_FIELD_TRAILING_ICON, new FlatSVGIcon("minisoccerfieldmanagement/drawer/icon/search.svg", 0.35f));
+        //  Change scroll style
+        JScrollPane scroll = (JScrollPane) table.getParent().getParent();
+        scroll.setBorder(BorderFactory.createEmptyBorder());
+        scroll.getVerticalScrollBar().putClientProperty(FlatClientProperties.STYLE, ""
+                + "background:$Table.background;"
+                + "track:$Table.background;"
+                + "trackArc:999");
+
+        table.getTableHeader().putClientProperty(FlatClientProperties.STYLE_CLASS, "table_style");
+        table.putClientProperty(FlatClientProperties.STYLE_CLASS, "table_style");
+
+        //  To Create table alignment
+        table.getTableHeader().setDefaultRenderer(getAlignmentCellRender(table.getTableHeader().getDefaultRenderer(), true));
+        table.setDefaultRenderer(Object.class, getAlignmentCellRender(table.getDefaultRenderer(Object.class), false));
+    }
+
+    private TableCellRenderer getAlignmentCellRender(TableCellRenderer oldRender, boolean header) {
+        return new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                Component com = oldRender.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                if (com instanceof JLabel) {
+                    JLabel label = (JLabel) com;
+                    if (column == 0 || column == 4) {
+                        label.setHorizontalAlignment(SwingConstants.CENTER);
+                    } else if (column == 2 || column == 3) {
+                        label.setHorizontalAlignment(SwingConstants.TRAILING);
+                    } else {
+                        label.setHorizontalAlignment(SwingConstants.LEADING);
+                    }
+                    if (header == false) {
+                        if (column == 4) {
+                            if (Double.parseDouble(value.toString()) > 0) {
+                                com.setForeground(new Color(17, 182, 60));
+                                label.setText("+" + value);
+                            } else {
+                                com.setForeground(new Color(202, 48, 48));
+                            }
+                        } else {
+                            if (isSelected) {
+                                com.setForeground(table.getSelectionForeground());
+                            } else {
+                                com.setForeground(table.getForeground());
+                            }
+                        }
+                    }
+                }
+                return com;
+            }
+        };
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -41,63 +137,113 @@ public class ServiceManagement extends TabbedForm {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        flatLabel1 = new com.formdev.flatlaf.extras.components.FlatLabel();
-        flatTextField1 = new com.formdev.flatlaf.extras.components.FlatTextField();
+        crazyPanel1 = new raven.crazypanel.CrazyPanel();
+        crazyPanel2 = new raven.crazypanel.CrazyPanel();
+        txtSearch = new javax.swing.JTextField();
+        cmdAdd = new javax.swing.JButton();
+        cmdUpdate = new javax.swing.JButton();
+        cmdDelete = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
 
         setMaximumSize(new java.awt.Dimension(1188, 696));
         setPreferredSize(new java.awt.Dimension(1188, 696));
 
-        flatLabel1.setText("Name");
+        crazyPanel1.setFlatLafStyleComponent(new raven.crazypanel.FlatLafStyleComponent(
+            "background:$Table.background;[light]border:0,0,0,0,shade(@background,5%),,20;[dark]border:0,0,0,0,tint(@background,5%),,20",
+            null
+        ));
+        crazyPanel1.setMigLayoutConstraints(new raven.crazypanel.MigLayoutConstraints(
+            "wrap,fill,insets 15",
+            "[fill]",
+            "[grow 0][fill]",
+            null
+        ));
+
+        crazyPanel2.setFlatLafStyleComponent(new raven.crazypanel.FlatLafStyleComponent(
+            "background:$Table.background",
+            new String[]{
+                "JTextField.placeholderText=Search;background:@background",
+                "background:lighten(@background,8%);borderWidth:1",
+                "background:lighten(@background,8%);borderWidth:1",
+                "background:lighten(@background,8%);borderWidth:1"
+            }
+        ));
+        crazyPanel2.setMigLayoutConstraints(new raven.crazypanel.MigLayoutConstraints(
+            "",
+            "[]push[][]",
+            "",
+            new String[]{
+                "width 200"
+            }
+        ));
+        crazyPanel2.add(txtSearch);
+
+        cmdAdd.setText("Add New");
+        crazyPanel2.add(cmdAdd);
+
+        cmdUpdate.setText("Update");
+        crazyPanel2.add(cmdUpdate);
+
+        cmdDelete.setText("Delete");
+        crazyPanel2.add(cmdDelete);
+
+        crazyPanel1.add(crazyPanel2);
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "", "Name", "Balance", "Other", "Status"
             }
-        ));
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Boolean.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class
+            };
+            boolean[] canEdit = new boolean [] {
+                true, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         jScrollPane1.setViewportView(jTable1);
+
+        crazyPanel1.add(jScrollPane1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(120, 120, 120)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 646, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(53, 53, 53)
-                .addComponent(flatLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(flatTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 289, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(259, Short.MAX_VALUE))
+                .addGap(113, 113, 113)
+                .addComponent(crazyPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(368, 368, 368))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(117, 117, 117)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(161, 161, 161)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(flatLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(flatTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(152, Short.MAX_VALUE))
+                .addGap(94, 94, 94)
+                .addComponent(crazyPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 583, Short.MAX_VALUE)
+                .addGap(19, 19, 19))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private com.formdev.flatlaf.extras.components.FlatLabel flatLabel1;
-    private com.formdev.flatlaf.extras.components.FlatTextField flatTextField1;
+    private javax.swing.JButton cmdAdd;
+    private javax.swing.JButton cmdDelete;
+    private javax.swing.JButton cmdUpdate;
+    private raven.crazypanel.CrazyPanel crazyPanel1;
+    private raven.crazypanel.CrazyPanel crazyPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JTextField txtSearch;
     // End of variables declaration//GEN-END:variables
 }
