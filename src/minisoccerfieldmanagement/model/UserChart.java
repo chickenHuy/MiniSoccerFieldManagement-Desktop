7122c22277ac6cc3378f5d8b4f5a50c6cddebf0f
Tran Lam Nhut Khang <92084108+Khan9Tran@users.noreply.github.com>
2024-04-20 13:55:26 +0700

    xóa png dư

---
+++ src/minisoccerfieldmanagement/model/UserChart.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package minisoccerfieldmanagement.model;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author trank
+ */
+public class UserChart {
+    private BigDecimal price;
+    private String name;
+    private int userId;
+    private java.sql.Date date;
+}
