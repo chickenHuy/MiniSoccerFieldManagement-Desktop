42e65d989964bcfd99aa1aec196a63deb53955bc
thanhhuy25 <nguyenhuythanh256@gmail.com>
2024-04-25 00:11:31 +0700

    Tạo Interface gửi quantityOrder về xử lí

---
+++ src/minisoccerfieldmanagement/util/ISendQuantityOrder.java
@@ -0,0 +1,8 @@
+package minisoccerfieldmanagement.util;
+
+import minisoccerfieldmanagement.model.Service;
+
+public interface ISendQuantityOrder {
+
+    void sendQuantityOrder(Service service, int quantityOrder);
+}
